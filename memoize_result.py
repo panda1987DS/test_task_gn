742ae3db63cb4d6bb03a9b26ce620d93c3453c28
Dasha <pankratova.dasha@gmail.com>
2022-04-28 17:41:56 +0300

    Rerite to class, add test

---
+++ memoize_result.py
@@ -0,0 +1,71 @@
+from typing import Callable, Any
+from collections import OrderedDict
+from warnings import warn
+import logging
+import pickle
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename="memoize_result.log", level=logging.DEBUG)
+
+
+class Cache:
+    def __init__(self, function: Callable[..., Any], maxsize: int, dump_memo: bool) -> Any:
+        self.function = function
+        self.maxsize = maxsize
+        self.dump_memo = dump_memo
+        self.path = f"{function.__name__}.pickle"
+        if dump_memo:
+            self.load()
+        else:
+            self.memo = OrderedDict()
+
+    def __call__(self, *args, **kwargs):
+        try:
+            return self.memo[args]
+        except KeyError:
+            result = self.function(*args)
+            self.memo[args] = result
+            if self.dump_memo:
+                self.dump()
+            if not self.maxsize is None and len(self.memo) > self.maxsize:
+                self.memo.popitem(False)
+            return result
+        except TypeError:
+            warn("Results are not hashed. Can't work with unhashable type parameters. ", RuntimeWarning)
+            result = self.function(*args)
+            return result
+
+    def dump(self):
+        with open(self.path, 'wb') as handle:
+            pickle.dump(self.memo, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        logger.info(f"Save file {self.path}")
+
+    def load(self):
+        try:
+            with open(self.path, 'rb') as handle:
+                self.memo = pickle.load(handle)
+            logger.info(f"Load file {self.path}")
+        except FileNotFoundError:
+            self.memo = OrderedDict()
+            logger.info(f"File {self.path} not found")
+
+    def clean(self):
+        self.memo = OrderedDict()
+        if self.dump_memo:
+            self.dump()
+
+    def __len__(self):
+        return len(self.memo)
+
+
+    def print(self):
+        print(
+            f"function = {self.function.__name__} \nmemo = {self.memo} \ndump memo = {self.dump_memo} \nmaxsize = {self.maxsize}")
+
+
+def memoize_result(maxsize: int = None, dump_memo: bool = False):
+    def memoize_result_(function):
+        return Cache(function, maxsize, dump_memo)
+
+    return memoize_result_
+
---
+++ test_momoize_result.py
@@ -0,0 +1,62 @@
+import pytest
+from datetime import datetime
+from memoize_result import memoize_result
+import pickle
+
+@memoize_result()
+def default(par1, par2):
+    return datetime.now()
+
+
+@memoize_result()
+def for_clean(par1, par2):
+    return datetime.now()
+
+
+@memoize_result(maxsize=2)
+def max_size(par1):
+    return datetime.now()
+
+
+@memoize_result(dump_memo=True)
+def dump_memo(par1):
+    return datetime.now()
+
+
+def test_momoize_any():
+    result_1_1 = default(1, 2)
+    result_2_1 = default(3, 2)
+    result_1_2 = default(1, 2)
+    result_2_2 = default(3, 2)
+    assert result_1_1 == result_1_2
+    assert result_2_1 == result_2_2
+    assert result_1_1 != result_2_1
+
+
+def test_clean():
+    assert len(for_clean) == 0
+    for_clean(1, 2)
+    for_clean(3, 2)
+    assert len(for_clean) == 2
+    for_clean.clean()
+    assert len(for_clean) == 0
+
+
+def test_unhashable_type():
+     default({1: 1, 2: 2}, 2)
+
+
+def test_max_size():
+    result_1 = max_size(1)
+    result_2 = max_size(2)
+    assert result_1 == max_size(1)
+    result_3 = max_size(3)
+    assert result_1 != max_size(1)
+    assert result_3 == max_size(3)
+
+
+def test_dump_memo():
+    result = dump_memo("test")
+    with open(dump_memo.path, 'rb') as handle:
+        memo = pickle.load(handle)
+    assert result == memo[("test",)]
